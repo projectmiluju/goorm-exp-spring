f7b4d2530e80db875547c16db090bc584e23ce3f
projectmiluju <dnjsdyd0712@naver.com>
2024-11-08 07:31:29 +0900

    feat(스프링 입문): HelloController에서 HelloService를 사용하여 메시지 반환

--- practice/src/main/java/com/example/practice/controller/HelloController.java
+++ practice/src/main/java/com/example/practice/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.practice.controller;
 
+import com.example.practice.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final HelloService helloService;
+
+    // 생성자 주입
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return helloService.getHelloMessage();
     }
 }
