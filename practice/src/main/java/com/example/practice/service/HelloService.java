687b55b6c5a60b96c043630531b0cad1eb68b5fd
projectmiluju <dnjsdyd0712@naver.com>
2024-11-09 07:33:27 +0900

    feat(스프링 입문): HelloService에서 HelloRepository를 사용하여 메시지 반환 로직 수정

--- practice/src/main/java/com/example/practice/service/HelloService.java
+++ practice/src/main/java/com/example/practice/service/HelloService.java
@@ -1,11 +1,18 @@
 package com.example.practice.service;
 
+import com.example.practice.repository.HelloRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
 
+    private final HelloRepository helloRepository;
+
+    public HelloService(HelloRepository helloRepository) {
+        this.helloRepository = helloRepository;
+    }
+
     public String getHelloMessage() {
-        return "Hello, World!";
+        return helloRepository.findMessage();
     }
 }
