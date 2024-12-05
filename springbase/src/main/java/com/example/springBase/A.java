e2fa00b352ee6521b357cc012f6f057f179a866b
projectmiluju <dnjsdyd0712@naver.com>
2024-12-05 11:39:45 +0900

    feat(스프링 핵심 원리 기본): 순환 의존성 해결 (@Lazy 사용)

--- springbase/src/main/java/com/example/springBase/A.java
+++ springbase/src/main/java/com/example/springBase/A.java
@@ -1,5 +1,6 @@
 package com.example.springBase;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,11 +8,12 @@ public class A {
 
     private final B b;
 
-    public A(B b) {
+    public A(@Lazy B b) {
         this.b = b;
     }
 
     public void hello() {
         System.out.println("A가 호출됨");
+        b.hello(); // b도 호출 확인
     }
 }
