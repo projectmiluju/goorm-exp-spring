99edc48793ca3f0defe354cf23764e5a0b8a34a0
projectmiluju <dnjsdyd0712@naver.com>
2024-12-07 03:46:36 +0900

    feat(싱글톤 빈 스코프와 프로토타입 빈 스코프 구현하기): Singleton vs Prototype

--- springbase/src/main/java/com/example/springBase/AppRunner.java
+++ springbase/src/main/java/com/example/springBase/AppRunner.java
@@ -1,17 +1,28 @@
 package com.example.springBase;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements CommandLineRunner {
 
-    @Autowired
-    private A a;
+    private final ApplicationContext context;
+
+    public AppRunner(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Override
-    public void run(String... args) throws Exception {
-        a.hello();
+    public void run(String... args) {
+        System.out.println("==== SingletonBean 테스트 ====");
+        SingletonBean singleton1 = context.getBean(SingletonBean.class);
+        SingletonBean singleton2 = context.getBean(SingletonBean.class);
+        System.out.println("singleton1 == singleton2: " + (singleton1 == singleton2));
+
+        System.out.println("==== PrototypeBean 테스트 ====");
+        PrototypeBean proto1 = context.getBean(PrototypeBean.class);
+        PrototypeBean proto2 = context.getBean(PrototypeBean.class);
+        System.out.println("proto1 == proto2: " + (proto1 == proto2));
     }
 }
